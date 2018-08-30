b799219789c052562a8fc4d82e8a0379b8bd278d
paulsrijit <paul.srijit@gmail.com>
2018-08-30 18:56:23 +0530

    Another project pushed

---
+++ Java8Practice/src/LambdaExpressionExample.java
@@ -0,0 +1,22 @@
+
+public class LambdaExpressionExample {
+
+	public static void main(String[] args) {
+		/*MyInterface obj = new MyInterface() {
+			
+			@Override
+			public int add(int x, int y) {				
+				return x+y;
+			}
+		};*/
+		
+		MyInterface obj = (int x, int y) -> (x+y);
+		System.out.println(obj.add(2, 3));
+	}
+
+}
+
+interface MyInterface{
+	int add(int x, int y);
+}
+
