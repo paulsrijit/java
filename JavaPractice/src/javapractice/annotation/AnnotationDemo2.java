5c8d6eefaf3629ff386356bab459c89f01f39174
Srijit Paul <paul.srijit@gmail.com>
2018-09-11 18:14:43 +0530

    Ofc

---
+++ JavaPractice/src/javapractice/annotation/AnnotationDemo2.java
@@ -0,0 +1,10 @@
+package javapractice.annotation;
+
+public class AnnotationDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- JavaPractice/src/javapractice/annotation/MyAnnotation.java
+++ JavaPractice/src/javapractice/annotation/MyAnnotation.java
@@ -1,5 +1,18 @@
 package javapractice.annotation;
 
-public @interface MyAnnotation {
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnnotation {
+	String antNm() default "DefaultAnnotationName";
+	int antVal() default -1;
+}
+
+class TestClass {
+	public void myfuntion() {
+		System.out.println("Hello from myfunction");
+	}
+	
+	
 }
