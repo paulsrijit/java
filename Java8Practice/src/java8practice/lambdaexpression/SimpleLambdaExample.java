4e3df194fe9ffa2fa7b59d15947af82990c7a26d
Srijit Paul <paul.srijit@gmail.com>
2018-09-25 09:04:41 +0530

    WAH

--- Java8Practice/src/java8practice/lambdaexpression/SimpleLambdaExample.java
+++ Java8Practice/src/java8practice/lambdaexpression/SimpleLambdaExample.java
@@ -8,10 +8,20 @@ public class SimpleLambdaExample {
 		
 		NumericTest makeDouble = (n) -> (n*2);
 		System.out.println(makeDouble.operate(10));
+		
+		StringOperation addDash = (s) -> (s+"-");
+		System.out.println(addDash.operate("Hello"));
+		
+		StringOperation addSurname = (s) -> (s+ " Paul");
+		System.out.println(addSurname.operate("Srijit"));
 	}
 
 }
 
 interface NumericTest {
 	int operate(int n);
+}
+
+interface StringOperation {
+	String operate(String s);
 }
